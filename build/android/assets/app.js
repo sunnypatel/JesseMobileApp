0e01f6d38dba5df88ee622ad7f3aca6da2164fc3
Daniel <dan.molander@gmail.com>
2014-12-26 13:49:31 -0500

    initial commit from restucturing of JesseMobile

--- build/android/assets/app.js
+++
@@ -1,6 +0,0 @@
- // this sets the background color of the master UIView (when there are no windows/tab groups on it)
-Titanium.UI.setBackgroundColor('#000');
-
-Ti.include("state/StateManager.js");
-
-StateManager.getInstance().setState("login");
