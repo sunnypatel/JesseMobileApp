124fd500a3973e9d9f7bb893c11daf746129bb76
Sunny Patel <spatel@leadnomics.com>
2015-01-18 13:48:08 -0500

    Fix login status 0

--- app/lib/APIService.js
+++ app/lib/APIService.js
@@ -7,69 +7,56 @@
  * api.login(username, password, callback);
  */
 
-var url = "178.18.16.226:2730";
+var url = "http://178.18.16.226:2730";
 var token;
 
 exports.login = function(username, password, callback){	
 	var json;
-	var client = Ti.Network.createHTTPClient({
-		
-		// timeout:5000, //5 second timeout
-		
-		//if the login was successful
-		onload: function() {
-			json = JSON.parse(this.responseText);
-			token =  json.apiToken;
-			Ti.API.info("new token saved: "+token);
-			// StateManager.getInstance().setState("ordering");
-			callback(true);
-		},
-		//if the login was unsuccessful
-		onerror: function(e){
-			Ti.API.debug(e.error);
-			//connection failed
-			if(this.status == 0){
-				var toast = Ti.UI.createNotification({
-			    message:"Connection to "+url+" refused",	
-			    duration: Ti.UI.NOTIFICATION_DURATION_LONG
-				});
-				toast.show();
-			}
-			//TODOL when is this returned?
-			//not found
-			else if(this.status == 404){
-				var toast = Ti.UI.createNotification({
-			    message:"Invalid Username or Password",
-			    duration: Ti.UI.NOTIFICATION_DURATION_LONG
-				});
-				toast.show();
-			}
-			//TODO: when is this code returned?
-			else if(this.status == 401){
-				var toast = Ti.UI.createNotification({
-			    message:"Invalid Username or Password",
-			    duration: Ti.UI.NOTIFICATION_DURATION_LONG
-				});
-				toast.show();
-			}
-			//server error response
-			else if(this.status == 500){
-				var toast = Ti.UI.createNotification({
-			    message:"We have some technical difficulties :( try again later?)",
-			    duration: Ti.UI.NOTIFICATION_DURATION_LONG
-				});
-				toast.show();
-			}
-			callback(false);
-		}
-	});
+	var client = Ti.Network.createHTTPClient();
+	
 	//open the connection to the url designated
 	client.open("POST", url+"/user/login");
-	var data = {
-		"phone":username,
-		"password":password
+	client.setRequestHeader('Content-Type', 'application/json');
+	
+	// **************************************************
+	// *     IMPORTANT TO USE JSON.STRINGIFY FOR iOS    *
+	// **************************************************
+	client.send(JSON.stringify({
+		"phone": "2154590332",
+		"password": "password"
+	}));
+	
+	//if the login was successful
+	client.onload = function() {
+		Ti.API.debug('In onLoad');
+		json = JSON.parse(this.responseText);
+		token =  json.apiToken;
+		Ti.API.info("new token saved: "+token);
+		callback(true);
 	};
-	client.send(data);
+		//if the login was unsuccessful
+	client.onerror = function(e) {
+		Ti.API.debug(e.error);
+		//connection failed
+		if(this.status == 0){
+			Ti.API.debug(this);
+			alert("Connection to "+url+" refused");
+		}
+		//TODOL when is this returned?
+		//not found
+		else if(this.status == 404){
+			alert("Invalid Phone/Password combo");
+		}
+		//TODO: when is this code returned?
+		else if(this.status == 401){
+			alert("Invalid Phone/Password combo");
+		}
+		//server error response
+		else if(this.status == 500){
+		    alert("We're having technical difficulties");
+		}
+		callback(false);
+	};
 };
 
 exports.getRestaurantsByGeolocation = function(callback){	
@@ -88,36 +75,25 @@ exports.getRestaurantsByGeolocation = function(callback){
 			Ti.API.debug(e.error);
 			//connection failed
 			if(this.status == 0){
-				var toast = Ti.UI.createNotification({
-			    message:"Connection to "+url+" refused",	
-			    duration: Ti.UI.NOTIFICATION_DURATION_LONG
-				});
-				toast.show();
+				alert('Connection to "+url+" refused"');
 			}
 			//TODO when is this returned?
 			//not found
 			else if(this.status == 404){
-				var toast = Ti.UI.createNotification({
-			    message:"Invalid Username or Password",
-			    duration: Ti.UI.NOTIFICATION_DURATION_LONG
-				});
-				toast.show();
+				alert('Invalid Phone/Password combo')
 			}
 			//TODO: when is this code returned?
 			else if(this.status == 401){
 				var toast = Ti.UI.createNotification({
-			    message:"Invalid Username or Password",
+			    message:"",
 			    duration: Ti.UI.NOTIFICATION_DURATION_LONG
 				});
 				toast.show();
+				alert('Invalid Phone/Password combo')
 			}
 			//server error response
 			else if(this.status == 500){
-				var toast = Ti.UI.createNotification({
-			    message:"We have some technical difficulties :( try again later?)",
-			    duration: Ti.UI.NOTIFICATION_DURATION_LONG
-				});
-				toast.show();
+				alert("We're having technical difficulties");
 			}
 			callback(false);
 		}
@@ -128,7 +104,7 @@ exports.getRestaurantsByGeolocation = function(callback){
 		"latitude":location.latitude,
 		"longitude":location.longitude
 	};
-	client.send(data);
+	client.send(JSON.stringify(data));
 };
 
 
@@ -146,36 +122,20 @@ exports.getRestaurantById = function(id, callback){
 			Ti.API.debug(e.error);
 			//connection failed
 			if(this.status == 0){
-				var toast = Ti.UI.createNotification({
-			    message:"Connection to "+url+" refused",	
-			    duration: Ti.UI.NOTIFICATION_DURATION_LONG
-				});
-				toast.show();
+				alert('Connection refused');
 			}
 			//TODO when is this returned?
 			//not found
 			else if(this.status == 404){
-				var toast = Ti.UI.createNotification({
-			    message:"Invalid Username or Password",
-			    duration: Ti.UI.NOTIFICATION_DURATION_LONG
-				});
-				toast.show();
+				alert('Invalid Phone/Password combo');
 			}
 			//TODO: when is this code returned?
 			else if(this.status == 401){
-				var toast = Ti.UI.createNotification({
-			    message:"Invalid Username or Password",
-			    duration: Ti.UI.NOTIFICATION_DURATION_LONG
-				});
-				toast.show();
+				alert('Invalid Phone/Password combo');
 			}
 			//server error response
 			else if(this.status == 500){
-				var toast = Ti.UI.createNotification({
-			    message:"We have some technical difficulties :( try again later?)",
-			    duration: Ti.UI.NOTIFICATION_DURATION_LONG
-				});
-				toast.show();
+				alert("We're having technical difficulties");
 			}
 			callback(false);
 		}
